2fbf56665180a21a718be4d5f8128d075ce1c3f4
unknown <aq.syed007@gmail.com>
2021-05-18 16:29:08 +0500

    Starting project

---
+++ email_script.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.message import EmailMessage
+
+# with open('textfile.txt') as text:
+# 	email = EmailMessage()
+# 	email.set_content(text.read()) 
+
+email = EmailMessage()
+email['from'] = 'Spider'
+email['to'] = 'aq.syed007@gmail.com'
+email['subject'] = 'You won 1,000,000 dollars!'
+email.set_content('I\'m a unhatched programmer!')
+
+
+with smtplib.SMTP(host = 'smtp.gmail.com', port = 587) as smtp:
+	smtp.ehlo()
+	smtp.starttls()
+	smtp.login('aq.syed007@gmail.com', 'mySuperXecret@2021')
+	smtp.send_message(email)
+	print('all good boss!')
